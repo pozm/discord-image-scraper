0a119ff575d082354d8d79e2e6a505ec81ebaf19
Luna <44528100+pozm@users.noreply.github.com>
2022-03-01 21:42:54 +0000

    pain

--- src/app.rs
+++ src/app.rs
@@ -1,4 +1,15 @@
+use std::future::Future;
+use eframe::egui::TextEdit;
 use eframe::{egui, epi};
+use poll_promise::Promise;
+use serenity::{Client, FutureExt,async_trait};
+use serenity::client::EventHandler;
+use short_crypt::ShortCrypt;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {}
 
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[cfg_attr(feature = "persistence", derive(serde::Deserialize, serde::Serialize))]
@@ -9,15 +20,35 @@ pub struct TemplateApp {
 
     // this how you opt-out of serialization of a member
     #[cfg_attr(feature = "persistence", serde(skip))]
+    encryption : ShortCrypt,
     value: f32,
+    show_settings: bool,
+    #[cfg_attr(feature = "persistence", serde(skip))]
+    discord_token: String,
+    stored_discord_token: String,
+    download_to: String,
+    #[cfg_attr(feature = "persistence", serde(skip))]
+    discord_client:Option<Promise<Client>>
 }
 
 impl Default for TemplateApp {
     fn default() -> Self {
+        let encrypt_key = "poggers";
         Self {
-            // Example stuff:
+            encryption: ShortCrypt::new(encrypt_key),
+
+
             label: "Hello World!".to_owned(),
             value: 2.7,
+            show_settings: false,
+            discord_token: String::new(),
+            stored_discord_token: String::new(),
+            download_to: std::env::current_dir()
+                .unwrap()
+                .display()
+                .to_string()
+                .to_owned(),
+            discord_client:None
         }
     }
 }
@@ -34,11 +65,15 @@ impl epi::App for TemplateApp {
         _frame: &epi::Frame,
         _storage: Option<&dyn epi::Storage>,
     ) {
+        println!("setup!!");
+        println!("{}", cfg!(feature = "persistence"));
         // Load previous app state (if any).
         // Note that you must enable the `persistence` feature for this to work.
         #[cfg(feature = "persistence")]
         if let Some(storage) = _storage {
-            *self = epi::get_value(storage, epi::APP_KEY).unwrap_or_default()
+            println!("loading state");
+            *self = epi::get_value(storage, epi::APP_KEY).unwrap_or_default();
+            self.discord_token = String::from_utf8((*self.encryption.decrypt_qr_code_alphanumeric(&*self.stored_discord_token).unwrap_or_default()).to_owned()).unwrap()
         }
     }
 
@@ -46,13 +81,23 @@ impl epi::App for TemplateApp {
     /// Note that you must enable the `persistence` feature for this to work.
     #[cfg(feature = "persistence")]
     fn save(&mut self, storage: &mut dyn epi::Storage) {
+        self.stored_discord_token = self.encryption.encrypt_to_qr_code_alphanumeric(&self.discord_token);
         epi::set_value(storage, epi::APP_KEY, self);
     }
 
     /// Called each time the UI needs repainting, which may be many times per second.
     /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
     fn update(&mut self, ctx: &egui::Context, frame: &epi::Frame) {
-        let Self { label, value } = self;
+        let Self {
+            label,
+            value,
+            show_settings,
+            discord_token,
+            download_to,
+            encryption,
+            stored_discord_token,
+            discord_client
+        } = self;
 
         // Examples of how to create different panels and windows.
         // Pick whichever suits you.
@@ -67,6 +112,12 @@ impl epi::App for TemplateApp {
                         frame.quit();
                     }
                 });
+                ui.menu_button("Settings", |ui| {
+                    if ui.button("Toggle").clicked() {
+                        *show_settings = !*show_settings;
+                        ui.close_menu()
+                    }
+                })
             });
         });
 
@@ -96,23 +147,39 @@ impl epi::App for TemplateApp {
 
         egui::CentralPanel::default().show(ctx, |ui| {
             // The central panel the region left after adding TopPanel's and SidePanel's
+            ui.close_menu();
+            if discord_client.is_none() {
+                if ui.button("Login to discord").clicked() {
+                    discord_client.get_or_insert_with(|| {
+                        let (sender, prom) = Promise::new();
+                        let clientb = Client::builder(&discord_token).event_handler(Handler);
+                        clientb.then(|client| async move {
+                            println!("log in poggers");
+                            sender.send(client.expect("bruh"))
+                        }).;
+                        prom
+                    });
+                };
 
-            ui.heading("eframe template");
-            ui.hyperlink("https://github.com/emilk/eframe_template");
-            ui.add(egui::github_link_file!(
-                "https://github.com/emilk/eframe_template/blob/master/",
-                "Source code."
-            ));
+            } else {
+
+            }
             egui::warn_if_debug_build(ui);
         });
 
-        if false {
-            egui::Window::new("Window").show(ctx, |ui| {
-                ui.label("Windows can be moved by dragging them.");
-                ui.label("They are automatically sized based on contents.");
-                ui.label("You can turn on resizing and scrolling if you like.");
-                ui.label("You would normally chose either panels OR windows.");
+        // if *show_settings {
+        egui::Window::new("Settings")
+            .open(show_settings)
+            .show(ctx, |ui| {
+                ui.heading("Token");
+                ui.add(TextEdit::singleline(discord_token).hint_text("h"));
+                ui.heading("Download to");
+                if ui.button(format!("select [{}]", *download_to)).clicked() {
+                    if let Some(path) = rfd::FileDialog::new().pick_folder() {
+                        *download_to = path.display().to_string();
+                    }
+                }
             });
-        }
+        // }
     }
 }
